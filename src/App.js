bfb0e2b625779e79a5958968b68506502cb48210
BlackLotus96 <simonemeddi96@gmail.com>
2020-06-27 01:04:59 +0200

    commit Revisione

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import PrimaDeclinazione from "./components/PrimaDeclinazione";
+import Frase from "./components/Frase";
 
 function App() {
   return (
   <Router>
     <Route path="/" exact component={Home} />
-    <Route path="/play/prima_declinazione" exact component={PrimaDeclinazione}/>
+    <Route path="/play/prima_declinazione" component={PrimaDeclinazione}/>
   </Router>
   );
 }
